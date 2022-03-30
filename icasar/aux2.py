Fix bug in masking for one of figures.

icasar/aux2.py
@@ -243,13 +243,18 @@ def update_mask_sources_ifgs(mask_sources, sources, mask_ifgs, ifgs):
             ifgs_new_mask | r2 array | as per ifgs, but with a new mask.  
         History:
             2020/06/26 | MEG | Written
+            2022_03_30 | MEG |  fix a bug in the number of pixels for the output array (ie. number of columns of ifgs_new_mask)
         """
-        n_pixs_new = len(np.argwhere(mask_new == False))                                        
-        ifgs_new_mask = np.zeros((ifgs.shape[0], n_pixs_new))                        # initiate an array to store the modified sources as row vectors    
-        for ifg_n, ifg in enumerate(ifgs):                                 # Loop through each source
-            ifg_r2 = col_to_ma(ifg, mask_old)                             # turn it from a row vector into a rank 2 masked array        
-            ifg_r2_new_mask = ma.array(ifg_r2, mask = mask_new)              # apply the new mask   
-            ifgs_new_mask[ifg_n, :] = ma.compressed(ifg_r2_new_mask)       # convert to row vector and places in rank 2 array of modified sources
+
+        
+        for ifg_n, ifg in enumerate(ifgs):                                            # Loop through each source
+            ifg_r2 = col_to_ma(ifg, mask_old)                                         # turn it from a row vector into a rank 2 masked array        
+            ifg_r2_new_mask = ma.array(ifg_r2, mask = mask_new)                       # apply the new mask   
+            ifg_r1_new_mask = ma.compressed(ifg_r2_new_mask)                          # convert to row vector 
+            if ifg_n == 0:                                                            # if it's the first ifg..  
+                n_pixs_new = ifg_r1_new_mask.shape[0]                                 # get the new number of pixels 
+                ifgs_new_mask = np.zeros((ifgs.shape[0], n_pixs_new))                 # initiate an array of the correct size
+            ifgs_new_mask[ifg_n, :] = ifg_r1_new_mask                                 # put the row vector into the array
         return ifgs_new_mask
     
     

icasar/icasar_funcs.py
@@ -337,7 +337,6 @@ def ICASAR(n_comp, spatial_data = None, temporal_data = None, figures = "window"
         S_pca = S_pca[:n_comp,:]                                                                                                        # truncate to desired number of components
         A_pca =  A_pca[:,:n_comp]
         
-    
     if fig_kwargs['figures'] != "none":
         plot_pca_variance_line(PC_vals, title = '01_PCA_variance_line', **fig_kwargs)
         if spatial:
@@ -578,7 +577,7 @@ def LiCSBAS_to_ICASAR(LiCSBAS_out_folder, filtered = False, figures = False, n_c
         h5_file | string | path to h5 file.  e.g. cum_filt.h5
         figures | boolean | if True, make figures
         n_cols  | int | number of columns for figures.  May want to lower if plotting a long time series
-        crop_pixels | tuple | coords to crop images to.  x then y, 00 is top left.  e.g. (10, 500, 600, 900).  
+        crop_pixels | tuple | coords to crop images to.  x start x stop y start y stop , 00 is top left.  e.g. (10, 500, 600, 900).  
                                 x_start, x_stop, y_start, y_stop, No checking that inputted values make sense.  
                                 Note, generally better to have cropped (cliped in LiCSBAS language) to the correct area in LiCSBAS_for_LiCSAlert
         return_r3 | boolean | if True, the rank 3 data is also returns (n_ifgs x height x width).  Not used by ICASAR, so default is False
@@ -837,7 +836,7 @@ def LiCSBAS_to_ICASAR(LiCSBAS_out_folder, filtered = False, figures = False, n_c
     
     if mask_type == 'dem':
         mask = np.logical_or(mask_dem, mask_cum)                                                        # if dem, mask water (from DEM), and anything that's nan in cumulative (mask_cum)
-    elif mask_type == 'mask_licsbas':
+    elif mask_type == 'licsbas':
         mask = np.logical_or(mask_licsbas, np.logical_or(mask_dem, mask_cum))
         
     mask_r3 = np.repeat(mask[np.newaxis,], cumulative.shape[0], 0)